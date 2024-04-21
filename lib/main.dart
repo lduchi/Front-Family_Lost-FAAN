651ed662a8b672985380992220c8cb92817a24ab
Lokyre <jose.fares.est@tecazuay.edu.ec>
2024-04-21 00:10:21 -0500

    A

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,36 @@
-import 'package:familylost_faan/Screen/splash_screen.dart';
+import 'package:familylost_faan/pages/cubit/bottom_nav_cubit.dart';
+import 'package:familylost_faan/widgets/main_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const LostFamilyApp());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class LostFamilyApp extends StatelessWidget {
+  const LostFamilyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MySplashScreen(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromRGBO(13, 71, 161, 1),
+          brightness: Brightness.light,
+        ),
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Color.fromRGBO(13, 71, 161, 1),
+          brightness: Brightness.dark,
+        ),
+      ),
+      home: BlocProvider(
+        create: (context) => BottomNavCubit(),
+        child: const MainWrapper(),
+      ),
     );
   }
 }
