9ef778e21a12b0f1558bb98d783869800ebae6c5
Braulio Molina <68998494+brau5@users.noreply.github.com>
2024-05-04 17:30:11 -0500

    Add files via upload

---
+++ lib/widgets/app_bar/appbar_subtitle.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+import '../../theme/theme_helper.dart'; // ignore: must_be_immutable
+// ignore_for_file: must_be_immutable
+
+// ignore_for_file: must_be_immutable
+class AppbarSubtitle extends StatelessWidget {
+  AppbarSubtitle({Key? key, required this.text, this.margin, this.onTap})
+      : super(
+          key: key,
+        );
+
+  String text;
+
+  EdgeInsetsGeometry? margin;
+
+  Function? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onTap?.call();
+      },
+      child: Padding(
+        padding: margin ?? EdgeInsets.zero,
+        child: Text(
+          text,
+          style: theme.textTheme.titleLarge!.copyWith(
+            color: theme.colorScheme.primary,
+          ),
+        ),
+      ),
+    );
+  }
+}
