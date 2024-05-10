0bfd771fd2d80eec736e9229d60f28cff4a871d2
Juan <briones12152000@gmail.com>
2024-05-09 22:09:17 -0500

    model_login01

---
+++ lib/ServiciosApp/models/model_login.dart
@@ -0,0 +1,14 @@
+import 'dart:typed_data';
+
+class login {
+  int id = 0;
+  String l_nombre = "";
+  String l_username = "";
+  String l_password = "";
+
+  login({
+    required this.l_nombre,
+    required this.l_username,
+    required this.l_password,
+  });
+}
